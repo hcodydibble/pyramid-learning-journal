c7d51576931944866ea72858c1b55520d4604fa1
Hewlette Dibble <hcodydibble@gmail.com>
2017-10-30 15:56:42 -0700

    started d learning journal stuff

---
+++ learning_journal/views/default.py
@@ -0,0 +1,21 @@
+"""."""
+
+
+def list_view(response):
+    """List journal entries."""
+    pass
+
+
+def detail_view(response):
+    """Show a single journal entry."""
+    pass
+
+
+def create_view(response):
+    """For creating a new view."""
+    pass
+
+
+def update_view(response):
+    """For updating an existing view."""
+    pass
